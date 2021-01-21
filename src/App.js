7b5bd6188eb7693b63bf79130041baaa7aa0feb6
FarihulR <farihulrouf@gmail.com>
2021-01-21 23:02:11 +0700

    front member

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import React from 'react';
+//import './App.css';
+import './assets/css/style.css';
+
+function App() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="h1.text-6xl">Javascript React </h1>
+    </div>
+  );
+}
+
+export default App;
